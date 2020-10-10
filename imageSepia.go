930c165a36d18325d6a3eb547b1acd4a18e470ee
shota-tsuji <shotatuzi@gmail.com>
2020-10-10 12:42:07 +0900

    feat: function sepia

--- imageSepia.go
+++ imageSepia.go
@@ -8,6 +8,23 @@ import (
 	"os"
 )
 
+func sepia(src image.Image, dst *image.RGBA) {
+	for x := 0; x < src.Bounds().Max.X; x++ {
+		for y := 0; y < src.Bounds().Max.Y; y++ {
+			r, g, b, a := src.At(x, y).RGBA()
+			r, g, b, a = r>>8, g>>8, b>>8, a>>8
+			coef := 0.33
+			avg := coef*float64(r) + coef*float64(g) + coef*float64(b)
+			stride := dst.Stride
+			rect := src.Bounds()
+			dst.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4] = uint8(1.03 * avg)
+			dst.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+1] = uint8(0.8 * avg)
+			dst.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+2] = uint8(0.55 * avg)
+			dst.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+3] = uint8(a)
+		}
+	}
+}
+
 func main() {
 	src, err := os.Open("src.jpg")
 	defer src.Close()
@@ -21,27 +38,21 @@ func main() {
 	}
 
 	dstImage := image.NewRGBA(m.Bounds())
-	for x := 0; x < m.Bounds().Max.X; x++ {
-		for y := 0; y < m.Bounds().Max.Y; y++ {
-			r, g, b, a := m.At(x, y).RGBA()
-			r, g, b, a = r>>8, g>>8, b>>8, a>>8
-			coef := 0.33
-			avg := coef*float64(r) + coef*float64(g) + coef*float64(b)
-			//r, g, b, a := m.At(x, y).RGBA().(color.RGBA)
-			//dstImage.SetRGBA(x, y, color.RGBA{rgba[1], rgba[2], rgba[0], rgba[3]})
-			//dstImage.SetRGBA(x, y, rgba)
-			stride := dstImage.Stride
-			rect := m.Bounds()
-			dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4] = uint8(1.03 * avg)   // R <- G
-			dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+1] = uint8(0.8 * avg)  // G <- B
-			dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+2] = uint8(0.55 * avg) // B <- R
-			dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+3] = uint8(a)
-			//dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4] = uint8(rgba.G)   // R <- G
-			//dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+1] = uint8(rgba.B) // G <- B
-			//dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+2] = uint8(rgba.R) // B <- R
-			//dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+3] = uint8(rgba.A)
-		}
-	}
+	//for x := 0; x < m.Bounds().Max.X; x++ {
+	//	for y := 0; y < m.Bounds().Max.Y; y++ {
+	//		r, g, b, a := m.At(x, y).RGBA()
+	//		r, g, b, a = r>>8, g>>8, b>>8, a>>8
+	//		coef := 0.33
+	//		avg := coef*float64(r) + coef*float64(g) + coef*float64(b)
+	//		stride := dstImage.Stride
+	//		rect := m.Bounds()
+	//		dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4] = uint8(1.03 * avg)
+	//		dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+1] = uint8(0.8 * avg)
+	//		dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+2] = uint8(0.55 * avg)
+	//		dstImage.Pix[(y-rect.Min.Y)*stride+(x-rect.Min.X)*4+3] = uint8(a)
+	//	}
+	//}
+	sepia(m, dstImage)
 
 	dstFile, err := os.Create("dst.jpg")
 	defer dstFile.Close()
